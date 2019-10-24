c06b9a4030f0aca8feebdf8801add41fe0f8ba1e
Dan Reeves <you@example.com>
2019-10-24 12:55:54 -0400

    MVP DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,16 @@ evans = User.create(name: 'Evans')
 switch = System.create(name: 'Nintendo Switch', num_of_players: 4)
 ps4 = System.create(name:'Playstation 4',num_of_players: 4)
 xbox = System.create(name:'Xbox One', num_of_players: 4)
-gameboy = System.create(name: 'Gameboy Advance',num_of_players: 1)
+gameboy = System.create(name: 'Gameboy',num_of_players: 1)
 the_ds = System.create(name: 'Nintendo DS', num_of_players: 1)
 psp = System.create(name: 'Playstation Portable',num_of_players: 1)
 computer = System.create(name: 'Gaming PC',num_of_players: 1)
 gamecube = System.create(name: 'Gamecube',num_of_players: 4)
-dreamcast = System.create(name: 'dreamcast',num_of_players: 2)
+dreamcast = System.create(name: 'Dreamcast',num_of_players: 2)
 wii = System.create(name: 'Wii',num_of_players: 4)
 
 # #GAME INSTANCES
+#DAN
 smash_u = Game.create(title: 'Super Smash Bros. Ultimate', num_of_players:4, user_id:dan.id, system_id:switch.id)
 goose_game = Game.create(title: 'Untitled Goose Game', num_of_players:1, user_id:dan.id, system_id:switch.id)
 fallout4 = Game.create(title: 'Fallout 4', num_of_players:1, user_id:dan.id, system_id:ps4.id)
@@ -29,41 +30,41 @@ ark = Game.create(title: 'Ark: Survival Evolved', num_of_players:1, user_id:dan.
 borderlands2 = Game.create(title: 'Borderlands 2', num_of_players:1, user_id:dan.id, system_id:computer.id)
 pokemon_ruby = Game.create(title: 'Pokemon Ruby', num_of_players:1, user_id:dan.id, system_id:gameboy.id)
 wii_sports = Game.create(title: 'Wii Sports', num_of_players:4, user_id:dan.id, system_id:wii.id)
-
+#KURAN
 smash_u_kuran = Game.create(title:'Super Smash Bros. Ultimate', num_of_players:4, user_id:kuran.id, system_id:switch.id)
 league_of_legends = Game.create(title:'League of Legends', num_of_players:1, user_id:kuran.id, system_id:computer.id)
 kingdom_hearts = Game.create(title:'Kingdom Hearts 3', num_of_players:1, user_id:kuran.id, system_id:ps4.id)
 bloodbourne = Game.create(title:'BLoodbourne', num_of_players:1, user_id:kuran.id, system_id:ps4.id)
 apex_legends = Game.create(title:'Apex Legends', num_of_players:1, user_id:kuran.id, system_id:computer.id)
 zelda = Game.create(title:'Zelda: Breathe of the Wild', num_of_players:1, user_id:kuran.id, system_id:switch.id)
-
+#CLARK
 smash_u_clark = Game.create(title:'Super Smash Bros. Ultimate', num_of_players:4 ,user_id:clark.id, system_id:switch.id)
 wii_sports_clark = Game.create(title:'Wii Sports', num_of_players:4, user_id:clark.id, system_id:wii.id)
 battlefield = Game.create(title:'Battlefield 3', num_of_players:1, user_id:clark.id, system_id:computer.id)
 forza = Game.create(title:'Forza Motorsport', num_of_players:1, user_id:clark.id, system_id:xbox.id)
 super_mario = Game.create(title:'Super Mario Galaxy 2', num_of_players:1, user_id:clark.id, system_id:wii.id)
-
+#JAVI
 rayman = Game.create(title:'Rayman Adventures', num_of_players:1, user_id:javi.id, system_id:the_ds.id)
 fast_n_furious = Game.create(title:'Fast N Furious', num_of_players:1, user_id:javi.id, system_id:psp.id)
 jak_n_daxter = Game.create(title:'Jak N Daxter', num_of_players:1, user_id:javi.id, system_id:psp.id)
 bomberman = Game.create(title:'Bomberman', num_of_players:1, user_id:javi.id, system_id:gameboy.id)
-
+#LYN
 m_kombat = Game.create(title:'Mortal Kombat', num_of_players:2, user_id:lyn.id, system_id:ps4.id)
 pokemon_red = Game.create(title:'Pokemon Red', num_of_players:1, user_id:lyn.id, system_id:ps4.id)
 just_dance = Game.create(title:'Just Dance', num_of_players:4, user_id:lyn.id, system_id:wii.id)
 wii_sports_lyn = Game.create(title:'Wii Sports', num_of_players:4, user_id:lyn.id, system_id:wii.id)
 wow = Game.create(title:'World of Warcraft', num_of_players:1, user_id:lyn.id, system_id:computer.id)
-
+#KAELAND
 pokemon_go = Game.create(title:'Pokemon.go', num_of_players:1, user_id:kaeland.id, system_id:switch.id)
 metroid = Game.create(title:'Metroid Prime', num_of_players:1, user_id:kaeland.id, system_id:gameboy.id)
 dd_adventures = Game.create(title:'Donald Duck Adventures', num_of_players:1, user_id:kaeland.id, system_id:gameboy.id)
 sonic = Game.create(title:'Sonic the Hedgehog', num_of_players:2, user_id:kaeland.id, system_id:dreamcast.id)
-
+#WILL
 dark_souls = Game.create(title:'Dark Souls 2', num_of_players:1, user_id:will.id, system_id:ps4.id)
 bloodbourne_will = Game.create(title:'Bloodbourne', num_of_players:1, user_id:will.id, system_id:ps4.id)
 zelda_will = Game.create(title:'Zelda: Breathe of the Wild', num_of_players:1, user_id:will.id, system_id:switch.id)
 battlefield4 = Game.create(title:'Battlefield 4', num_of_players:1, user_id:will.id, system_id:ps4.id)
-
+#EVANS
 smash_melee = Game.create(title:'Super Smash Bros. Melee', num_of_players:4, user_id:evans.id, system_id:gamecube.id)
 cod = Game.create(title:'Call of Duty: Black Ops 4', num_of_players:4, user_id:evans.id, system_id:xbox.id)
 gow = Game.create(title:'Gears of War', num_of_players:1, user_id:evans.id, system_id:xbox.id)
