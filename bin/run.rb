4266b55f24a41b686508e580d1ead59c7f26042d
Kuran <kurankumar@gmail.com>
2019-10-24 15:38:24 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,4 @@
-require_relative '../config/environment'
-
-
+require_relative "../config/environment"
 
 puts "HELLO WORLD"
+puts "Did project in our own directory. Didnt fork until we were already done :^) - Kuran and Clark"
